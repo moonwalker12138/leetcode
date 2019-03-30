ac18c6c42b5e8c8fe8dca5a934dab71ec037e288
Yang Fan <35723863+moonwalker12138@users.noreply.github.com>
2019-03-30 22:32:18 +0800

    update 279.perfect-squares.py

---
+++ 279.perfect-squares.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=279 lang=python3
+#
+# [279] Perfect Squares
+#
+# https://leetcode.com/problems/perfect-squares/description/
+#
+# algorithms
+# Medium (41.08%)
+# Total Accepted:    169.5K
+# Total Submissions: 411K
+# Testcase Example:  '12'
+#
+# Given a positive integer n, find the least number of perfect square numbers
+# (for example, 1, 4, 9, 16, ...) which sum to n.
+# 
+# Example 1:
+# 
+# 
+# Input: n = 12
+# Output: 3 
+# Explanation: 12 = 4 + 4 + 4.
+# 
+# Example 2:
+# 
+# 
+# Input: n = 13
+# Output: 2
+# Explanation: 13 = 4 + 9.
+#
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [0]
+        while len(dp) <= n:
+            dp += min(dp[-i*i] for i in range(1, int(len(dp)**0.5+1))) + 1,
+        return dp[n]
+
+if __name__=='__main__':
+    solution = Solution()
+    n = 28
+    res = solution.numSquares(n)
+    print(res)
