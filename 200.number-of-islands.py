488aababca7eebec23b1a9d72d0c6d3a9930b87e
Yang Fan <35723863+moonwalker12138@users.noreply.github.com>
2020-01-04 21:42:04 +0800

    update 200.number-of-islands.py

--- 200.number-of-islands.py
+++ 200.number-of-islands.py
@@ -44,57 +44,63 @@
 #
 
 # @lc code=start
-class UF:
-    def __init__(self, N):
-        self.pre = list(range(N))
-    def find(self, x):
-        # find root
-        r = x
-        while self.pre[r]!=r:
-            r = self.pre[r]
-        # compress path
-        cur = x
-        while cur!=r:
-            p = self.pre[cur]
-            self.pre[cur] = r
-            cur = p
-        return r
-    def union(self, x, y):
-        fx, fy = self.find(x), self.find(y)
-        if fx!=fy:
-            self.pre[fx] = fy
-from typing import List
-# find-union solution
-# class Solution:
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         if grid is None or len(grid)==0: return 0
-#         def get_id(row, col):
-#             return row * len(grid[0]) + col
-#         # convert matrix to edges representing connected islands
-#         edges = []
-#         for i in range(len(grid)):
-#             for j in range(len(grid[i])):
-#                 if grid[i][j]=="0": continue
-#                 cur_id = get_id(i ,j)
-#                 # self loop
-#                 edges.append((cur_id, cur_id))
-#                 for dir in [[1,0], [0,1]]:
-#                     x,y = i+dir[0], j+dir[1]
-#                     if 0<=x<len(grid) and 0<=y<len(grid[0]) and grid[x][y]=="1":
-#                         edges.append((cur_id, get_id(x,y)))
-#         # ? union-find init, too much memory usage
-#         uf = UF(len(grid) * len(grid[0]))
-#         for l,r in edges:
-#             uf.union(l,r)
-#         # count different root nodes
-#         roots = set()
-#         for l,r in edges:
-#             roots.add(uf.find(l))
-#             roots.add(uf.find(r))
-#         ans = len(roots)
-#         return ans
+""" union-find solution """
+class UnionFind:
+    def __init__(self, grid):
+        row, col = len(grid), len(grid[0])
+        self.parent = [None] * (row * col)
+        self.rank = [1] * (row * col)
+        self._size = 0
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j]=="1":
+                    self.parent[i*col+j] = i*col+j
+                    self._size += 1
 
-# dfs solution
+    def find(self, x):
+        """ find with path compression """
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        """ union with rank """
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x!=root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            elif self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            else:
+                self.parent[root_x] = root_y
+                self.rank[root_y] += 1
+            self._size -= 1
+
+    @property
+    def size(self):
+        return self._size
+
+from typing import List
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if grid is None or len(grid)==0: return 0
+        uf = UnionFind(grid)
+
+        def get_id(row, col):
+            return row * len(grid[0]) + col
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j]=="1":
+                    cur_id = get_id(i,j)
+                    for dir in [[1,0], [0,1], [-1,0], [0,-1]]:
+                        x,y = i+dir[0], j+dir[1]
+                        if 0<=x<len(grid) and 0<=y<len(grid[0]) and grid[x][y]=="1":
+                            uf.union(cur_id, get_id(x,y))
+
+        return uf.size
+
+""" dfs solution """
 # class Solution:
 #     def numIslands(self, grid: List[List[str]]) -> int:
 #         def dfs(i, j, grid):
@@ -113,29 +119,29 @@ from typing import List
 
 #         return num
 
-# bfs solution
-from queue import Queue
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        def bfs(i, j, grid):
-            q = Queue()
-            q.put((i,j))
-            grid[i][j] = "0"
-            while not q.empty():
-                l,r = q.get()
-                for dir in [[1,0],[-1,0],[0,1],[0,-1]]:
-                    x,y = l+dir[0], r+dir[1]
-                    if 0<=x<len(grid) and 0<=y<len(grid[0]) and grid[x][y]=="1":
-                        q.put((x,y))
-                        grid[x][y] = "0"
+""" bfs solution """
+# from queue import Queue
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         def bfs(i, j, grid):
+#             q = Queue()
+#             q.put((i,j))
+#             grid[i][j] = "0"
+#             while not q.empty():
+#                 l,r = q.get()
+#                 for dir in [[1,0],[-1,0],[0,1],[0,-1]]:
+#                     x,y = l+dir[0], r+dir[1]
+#                     if 0<=x<len(grid) and 0<=y<len(grid[0]) and grid[x][y]=="1":
+#                         q.put((x,y))
+#                         grid[x][y] = "0"
 
-        num = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=="1":
-                    bfs(i, j, grid)
-                    num += 1
-        return num
+#         num = 0
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 if grid[i][j]=="1":
+#                     bfs(i, j, grid)
+#                     num += 1
+#         return num
 
 if __name__ == "__main__":
     grid = [["1","1","1"],["0","1","0"],["1","1","1"]]
