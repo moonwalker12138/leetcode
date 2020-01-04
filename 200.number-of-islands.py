caac60ddc26732601797be42b078de20cf268ee6
Yang Fan <35723863+moonwalker12138@users.noreply.github.com>
2020-01-04 20:46:01 +0800

    update 200.number-of-islands.py

--- 200.number-of-islands.py
+++ 200.number-of-islands.py
@@ -17,30 +17,30 @@
 # islands. An island is surrounded by water and is formed by connecting
 # adjacent lands horizontally or vertically. You may assume all four edges of
 # the grid are all surrounded by water.
-# 
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input:
 # 11110
 # 11010
 # 11000
 # 00000
-# 
+#
 # Output: 1
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input:
 # 11000
 # 11000
 # 00100
 # 00011
-# 
+#
 # Output: 3
-# 
+#
 #
 
 # @lc code=start
@@ -95,29 +95,53 @@ from typing import List
 #         return ans
 
 # dfs solution
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         def dfs(i, j, grid):
+#             grid[i][j] = "0"
+#             for dir in [[1,0],[-1,0],[0,1],[0,-1]]:
+#                 x,y = i+dir[0], j+dir[1]
+#                 if 0<=x<len(grid) and 0<=y<len(grid[0]) and grid[x][y]=="1":
+#                     dfs(x, y, grid)
+
+#         num = 0
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 if grid[i][j]=="1":
+#                     dfs(i, j, grid)
+#                     num += 1
+
+#         return num
+
+# bfs solution
+from queue import Queue
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i, j, grid):
+        def bfs(i, j, grid):
+            q = Queue()
+            q.put((i,j))
             grid[i][j] = "0"
-            for dir in [[1,0],[-1,0],[0,1],[0,-1]]:
-                x,y = i+dir[0], j+dir[1]
-                if 0<=x<len(grid) and 0<=y<len(grid[0]) and grid[x][y]=="1":
-                    dfs(x, y, grid)
+            while not q.empty():
+                l,r = q.get()
+                for dir in [[1,0],[-1,0],[0,1],[0,-1]]:
+                    x,y = l+dir[0], r+dir[1]
+                    if 0<=x<len(grid) and 0<=y<len(grid[0]) and grid[x][y]=="1":
+                        q.put((x,y))
+                        grid[x][y] = "0"
 
         num = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]=="1":
-                    dfs(i, j, grid)
+                    bfs(i, j, grid)
                     num += 1
-            
         return num
 
 if __name__ == "__main__":
-    grid = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+    grid = [["1","1","1"],["0","1","0"],["1","1","1"]]
     ans = Solution().numIslands(grid)
     print(ans)
 
-        
+
 # @lc code=end
 
